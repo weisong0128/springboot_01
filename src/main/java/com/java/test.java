1f5d2f428c27cd93c10a2b94d406c507e0423879
“weisong” <450428539@qq.com>
2019-11-15 23:48:58 +0800

    MD5Tool

--- src/main/java/com/java/test.java
+++
@@ -1,9 +0,0 @@
-package com.java;
-
-/**
- * description: 描述
- * author: ws
- * time: 2019/11/15 22:55
- */
-public class test {
-}
---
+++ src/main/java/com/java/utils/MD5Tool.java
@@ -0,0 +1,9 @@
+package com.java.utils;
+
+/**
+ * description: 描述
+ * author: ws
+ * time: 2019/11/15 23:13
+ */
+public class MD5Tool {
+}
