f9acc4d0c3b5c845acb493e84de03f9f2ad5a1b1
“weisong” <450428539@qq.com>
2019-11-18 23:07:49 +0800

    20191118

--- src/main/java/com/java/config/SwaggerConfig.java
+++ src/main/java/com/java/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.java.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * description:
  * author: ws
  * time: 2019/11/18 22:56
  */
+@Configuration
 public class SwaggerConfig {
 }
--- src/main/java/com/java/controller/Test.java
+++ src/main/java/com/java/controller/Test.java
@@ -1,9 +1,20 @@
 package com.java.controller;
 
+import com.java.service.UserService;
+import com.java.service.impl.UserServiceImpl;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * description:
  * author: ws
  * time: 2019/11/18 21:29
  */
 public class Test {
+    public static void main(String[] args) {
+        UserService u = new UserServiceImpl();
+        List<Map<String, Object>> maps = u.selectAllUsers();
+        System.out.println(maps);
+    }
 }
